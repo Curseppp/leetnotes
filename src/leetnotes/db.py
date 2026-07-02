5b0fef505f1c213aa0e2bd692e7d23d785f4d495
Curseppp <imenzorus@gmail.com>
2026-07-02 18:46:10 +0300

    (refactor): The database will now be located in the user's home folder, inside a hidden folder.

--- src/leetnotes/db.py
+++ src/leetnotes/db.py
@@ -1,13 +1,15 @@
 import sqlite3
 from pathlib import Path
 
-from .models import Difficulty, PublicProblem, Status, Problem
+from .models import Difficulty, PublicProblem, Status
 
-DB_PATH = Path("leetnotes.db")
+DB_PATH = Path.home() / ".leetnotes" / "leetnotes.db"
 
 
 
 def get_connection() -> sqlite3.Connection:
+    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
+
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
